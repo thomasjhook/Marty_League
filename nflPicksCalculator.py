86e02266f0005e55e2bd8706dad72feec69f7239
thomasjhook <hooktommy02@gmail.com>
2020-10-04 22:04:25 -0500

    starting framework for confidence pick algorithm rather than plain 1 point per correct pick

--- nflPicksCalculator.py
+++ nflPicksCalculator.py
@@ -5,6 +5,8 @@ from colorama import init, Fore, Style
 init()
 
 NUMBER_OF_GAMES = 10
+FIRST_AMOUNT = 200
+SECOND_AMOUNT = 60
 
 def main(inFile, verbose):
 
@@ -61,8 +63,12 @@ def displayParticipantsPicks(answersDict):
 # Helper function to return a string of values seperated by a comma and space, given a list.
 def buildPickString(answerList):
     pickStr = ""
-    for x in answerList:
-        pickStr = pickStr + x + ", "
+    for x in range(len(answerList) - 1):
+        if x % 2 == 0:
+            pickStr = pickStr + answerList[x] + ", "
+    
+    # Take away last comma on end of string
+    pickStr = pickStr[:-2]
 
     return pickStr
 
@@ -96,29 +102,28 @@ def buildResultsDict(answers):
     # the participant answer to the answer key to and store result in
     # another internal data structure (results)
     for x in answers:
-        numCorrect = 0
-        counter = 0
         if x != "answer" and x != "numGames":
-            for answer in answers[x]:
-                # Actual comparison to check participant x's
-                # answers against the answer key's answers
-                if (answer.lower() == answers["answer"][counter].lower()) and counter < int(answers["numGames"][0]):
-                    numCorrect = numCorrect + 1
-                
-                counter = counter + 1
+            numCorrect = 0
+            for y in range(int(answers["numGames"][0]) * 2):
+                if y % 2 == 0:
+                    # Actual comparison to check participant x's
+                    # answers against the answer key's answers
+                    if (answers[x][y].lower() == answers["answer"][y].lower()) and y < (int(answers["numGames"][0]) * 2):
+                        numCorrect = numCorrect + int(answers[x][y + 1])
+
             results[x] = numCorrect
     return results
 
 def calculateAndDisplayWinner(resultsDict, answersDict):
     localCount = 0
     prefix = "1st Place: "
-    amount = "$200"
-    tieBreakerScore = answersDict["answer"][int(answersDict["numGames"][0])]
+    amount = FIRST_AMOUNT
+    tieBreakerScore = answersDict["answer"][(int(answersDict["numGames"][0]) * 2)]
     while localCount < 2:
         print(Style.RESET_ALL)
         if localCount == 1:
             prefix = "2nd Place: "
-            amount = "$60"
+            amount = SECOND_AMOUNT
         winnersList = []
         smallestDiff = 100
         finalWinner = ""
@@ -129,11 +134,11 @@ def calculateAndDisplayWinner(resultsDict, answersDict):
                 winnersList.append({participant : resultsDict[participant]})
         
         if len(winnersList) > 1:
-            print("There is a tie with ", str(highestCorrectPicks[1])," correct picks. Result will be determined based on tie breaker score")
+            print("There is a tie with ", str(highestCorrectPicks[1])," points. Result will be determined based on tie breaker score")
             print("Tiebreaker score: " + str(tieBreakerScore))
             for winner in winnersList:
                 for winnerKey in winner:
-                    participantTieBreaker = answersDict[winnerKey][int(answersDict["numGames"][0])]
+                    participantTieBreaker = answersDict[winnerKey][(int(answersDict["numGames"][0])* 2)]
                     print(winnerKey + "'s tie breaker score: " + str(participantTieBreaker))
                     participantDiff = abs(int(participantTieBreaker) - int(tieBreakerScore))
                     if participantDiff <= smallestDiff:
@@ -144,13 +149,13 @@ def calculateAndDisplayWinner(resultsDict, answersDict):
                             smallestDiff = participantDiff
                             finalWinner = winnerKey
                             finalWinnerTB = str(participantTieBreaker)
-            print(Fore.GREEN + prefix + finalWinner + " wins ", amount, " with a tiebreaker score of " + finalWinnerTB)
+            print(Fore.GREEN + prefix + finalWinner + " wins ", "$" + str(amount), " with a tiebreaker score of " + finalWinnerTB + "!")
             resultsDict.pop(finalWinner)
             localCount = localCount + 1
         elif len(winnersList) == 1:
             for winner in winnersList:
                 for winnerKey in winner:
-                    print(Fore.GREEN + prefix + winnerKey + " wins ", amount, " with " + str(winner[winnerKey]) + " correct picks!")
+                    print(Fore.GREEN + prefix + winnerKey + " wins ", "$" + str(amount), " with " + str(winner[winnerKey]) + " points!")
                     resultsDict.pop(winnerKey)
                     localCount = localCount + 1
         else:
@@ -187,7 +192,7 @@ def runTestPool():
     while weekNum != 0:
 
         # Build the file name
-        fileName = "week_" + str(weekNum) + ".txt"
+        fileName = "cWeek_" + str(weekNum) + ".txt"
 
         # Call the functions to be tested to get the values to compare to global variables
         answers,results = initializeTestVariables(fileName)
@@ -262,16 +267,14 @@ def runSpellChecker(fileName):
     teamNames = ["cardinals","falcons","panthers","bears","cowboys","lions","pack","rams","vikings",
                  "saints","giants","eagles","49ers","seahawks","bucs","redskins","ravens","bills",
                  "bengals","browns","broncos","texans","colts","jags","chiefs","raiders","chargers",
-                 "dolphins","pats","jets","steelers","titans"]
-    participantNames = ["jason","austin","sam","fritzy","brad_j","tommy","rick","clark","carey",
-                        "nick","brownie","connor","marty","answer","numgames","empty"]
+                 "dolphins","pats","jets","steelers","titans","empty"]
     inFile = open(fileName)
     for line in inFile:
         line = line.strip()
         line = line.split(",")
-        for entry in line:
-            if entry.lower() not in participantNames and entry.lower() not in teamNames and entry != line[-1]:
-                print(Fore.RED + "You made a typo when entering " + entry + " for " + line[0] + " in " + fileName)
+        for x in range(len(line)):
+            if line[x].lower() not in teamNames and line[x] != line[-1] and x % 2 != 0:
+                print(Fore.RED + "You made a typo when entering " + line[x] + " for " + line[0] + " in " + fileName)
                 print(Style.RESET_ALL)
                 return False
 
@@ -283,4 +286,4 @@ def runSpellChecker(fileName):
 # runTestPool()
 
 # Call the main() function
-main("week_4.txt", False)
+main("cWeek_4.txt", False)
