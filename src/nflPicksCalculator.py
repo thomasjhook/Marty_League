043625856177741fadd9cc05b7aba995c0a3ed1a
thomasjhook <hooktommy02@gmail.com>
2022-10-12 20:25:10 -0500

    Pushing week 5

--- src/nflPicksCalculator.py
+++ src/nflPicksCalculator.py
@@ -542,7 +542,7 @@ def runInputValidator(fileName):
                  "kentucky","mich_st","iowa_st","rutgers","texas_tech","nc_st","tcu","wv","ohio_st","baylor","illinois","rutgers",
                  "auburn","az_st","usc","miss","unlv","wake_forest","hawaii","oregon","clemson","ucla", "florida_st",
                  "tennessee", "pitt", "houston", "kentucky", "stanford", "citadel", "etsu", "ole_miss", "ndsu","wku", "arizona",
-                 "georgia_tech", "osu", "kansas", "lsu"]
+                 "georgia_tech", "osu", "kansas", "lsu", "wmu", "emu"]
     participantNames = ["jason","austin","sam","fritzy","brad_j","tommy","rick","clark","carey",
                         "nick","brownie","connor","marty","answer","numgames","empty","jake_h","cal_griff",
                         "charlie","chubbs","skeeter", "format", "confidence", "no_confidence", "ron","brownie", "aj", "chris_q",
@@ -578,9 +578,9 @@ def runInputValidator(fileName):
 # runTestPool()
 
 # Validate inputs
-runInputValidator("week_4.txt")
+runInputValidator("week_5.txt")
 
 # Call the main() function
 # # # This needs to be commented out for unit tests to run properly
-main("week_4.txt", False)
+main("week_5.txt", False)
 
